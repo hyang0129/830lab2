ec8f7a03149ec5ea3ab695fff7c5edf26e607cec
hy3134 <hy3134@g.rit.edu>
2022-02-17 14:30:10 -0500

    abc

--- hop_ann.cu
+++ hop_ann.cu
@@ -115,7 +115,7 @@ int main(int argc, char** argv) {
 		}
 
 
-		printf("QUERY %d \n", index);
+		printf("QUERY %d \n", Q);
 
 		// explore for all nodes 
 		vector<int> allPossibleNodes = explore(start_point, hop);
