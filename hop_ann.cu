ae7d4a89b8a09591d7a592586ac7967405e48cdc
hy3134 <hy3134@g.rit.edu>
2022-02-17 14:48:45 -0500

    abc

--- hop_ann.cu
+++ hop_ann.cu
@@ -116,7 +116,7 @@ int main(int argc, char** argv) {
 		}
 
 
-		//printf("QUERY %d \n", i);
+		printf("QUERY %d \n", i);
 
 		// explore for all nodes 
 		vector<int> allPossibleNodes = explore(start_point, hop);
@@ -165,7 +165,7 @@ int main(int argc, char** argv) {
 
 		//cuda 
 
-		int threadsPerBlock = 4;
+		int threadsPerBlock = 256;
 		int blocksPerGrid = (allPossibleNodes.size() + threadsPerBlock - 1) / threadsPerBlock;
 		//printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
 		cuda_squared_l2_dist <<<blocksPerGrid, threadsPerBlock>>> (query_data, targets, distances, D, allPossibleNodes.size());
